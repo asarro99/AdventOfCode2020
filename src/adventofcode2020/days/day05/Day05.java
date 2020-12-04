4a82bace7939ef0d43c29dfe9710db518a0c9992
Antonio Sarro <sarroantonio1999@gmail.com>
2020-12-04 16:44:54 +0100

    ✨ Prepare Day 5

---
+++ src/adventofcode2020/days/day05/Day05.java
@@ -0,0 +1,10 @@
+package adventofcode2020.days.day05;
+
+import adventofcode2020.templates.Day;
+
+public class Day05 extends Day {
+  @Override
+  protected Result evaluate() {
+    return new Result(0, 0);
+  }
+}
